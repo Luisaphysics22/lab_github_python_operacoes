d960051cfd70e9c965866ce236b3505661229a05
Luisa Lemos Teixeira <150837960+Luisaphysics22@users.noreply.github.com>
2024-08-08 11:50:08 +0000

    add repetindo textos

---
+++ repetindo_textos/repetindo_txt.py
@@ -0,0 +1,11 @@
+# Receber a string do usuário
+string = input("Digite uma string: ")
+
+# Receber o número inteiro do usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Repetir a string o número de vezes informado
+resultado = string * numero
+
+# Exibir o resultado
+print("A string repetida é:", resultado)
