c1b40726be94652a5e368f7bd3ae18607b30009e
Luisa Lemos Teixeira <150837960+Luisaphysics22@users.noreply.github.com>
2024-08-08 11:36:22 +0000

    add concatenando strings

---
+++ concatenando_strings/concatenando_str.py
@@ -0,0 +1,11 @@
+# Receber o primeiro dado do usuário
+dado1 = input("Digite o primeiro dado: ")
+
+# Receber o segundo dado do usuário
+dado2 = input("Digite o segundo dado: ")
+
+# Concatenar os dois dados com um espaço entre eles
+resultado = dado1 + " " + dado2
+
+# Exibir o resultado
+print("A string concatenada é:", resultado)
